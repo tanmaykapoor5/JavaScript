ebe07fd8b128782e9809298294f37b9384d5eb44
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-08-23 10:18:10 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 700655
+let accountEmail = "tanmay@gmai.com"
+var accountPassword = "12345"
+accountCity = "Jammu"
+let accountState
+
+// accountId = 2 // not allowed
+
+accountEmail = "ghd@gmail.com"
+accountPassword = "54321"
+accountCity = "Bangalore"
+
+
+console.table([accountEmail, accountId, accountCity, accountPassword, accountState])
+
+/*
+prefer not use var 
+beacuse of its block scope and functional scope
+
+*/
+
