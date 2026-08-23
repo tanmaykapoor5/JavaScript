63690b134cb3b53cd4590b21cb415c99c66703e3
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-08-23 15:42:16 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3+3)
+
+let name = "tanmay"
+let age = 19
+let isLoggedIn = false
+
+// number // range 2^53
+// bigint
+// string ""
+// boolean true or false
+// null standalone value
+// undefined
+// symbol
+
+// object
+
+console.log(typeof name)
