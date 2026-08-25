9da72d4c4b8afadb74908ff4f698ec5febe814e4
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-08-25 10:39:07 +0000

    conversions

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,25 @@
+// let score = "33"
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber)
+
+// let score = null
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+let score = undefined
+
+console.log(typeof score)
+
+let valueInNumber = Number(score) 
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+
