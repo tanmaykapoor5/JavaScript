fb60dd2acb28fcd53e06f273a8b4e0150560af12
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-08-25 12:07:31 +0000

    comparison and operators

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -14,12 +14,47 @@
 // console.log(valueInNumber)
 
 
-let score = undefined
+// let score = undefined
 
-console.log(typeof score)
+// console.log(typeof score)
 
-let valueInNumber = Number(score) 
-console.log(typeof valueInNumber)
-console.log(valueInNumber)
+// let valueInNumber = Number(score) 
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
 
+
+// ***********OPERATIONS ***************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+let str1 = "hello"
+let str2 = " Tanmay"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log(1 + "2")
+// console.log("1" + 2)
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+
+console.log(+true)
+console.log(+"")
+
+
+// let gamecounter = 100
+// gamecounter++
+// console.log(gamecounter)
+
+
+// let gamecounter = 100
+// ++gamecounter
+// console.log(gamecounter)
+
+let x = 3
+let y = x++
+console.log(x)
+console.log(y)
 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1)
+// console.log(2 >= 1)
+// console.log(2 < 1)
+// console.log(2 == 1)
+// console.log(2 != 1)
+
+console.log("2" > 1)
+console.log("02" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+/* comparison operator treat null as a number like 0 
+   and equality operator works differently
+
+*/
+
+// === also checks the data type it will not convert the datatype
