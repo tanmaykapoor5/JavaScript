aba1a7bbedaccfb10d00215233f513a0dadcd5ff
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-09-03 18:29:34 +0000

    sumaryofdatatypes

---
+++ 01_basics/datatypes_sumary.js
@@ -0,0 +1,16 @@
+// Primitive
+
+// 7 types : string, number, boolean, null, undefined, symbol, bigint
+
+// Reference (Non primitive)
+
+// Array, objects, functions
+
+let myObj = {
+    name: "Tanmay",
+    age: 19,
+
+}
+
+console.log(myObj)
+console.log(typeof myObj)
