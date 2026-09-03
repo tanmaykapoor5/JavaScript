655af099a0a4ff9efa7810282dd566401f3778db
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-09-03 18:52:21 +0000

    Memory

--- 01_basics/datatypes_sumary.js
+++ 01_basics/datatypes_sumary.js
@@ -14,3 +14,46 @@ let myObj = {
 
 console.log(myObj)
 console.log(typeof myObj)
+
+// ******* Memory in javascript *********
+
+// Stack memory (in primitive)
+// Heap memory  (in non primitive)
+
+let myname = "Tanmay"
+
+let myfriendname = myname  
+myfriendname = "Ronit"
+
+/*
+in stack you will get the copy of 
+value so if you change the value 
+in new variable it wont affect the original variable value
+*/
+
+console.log(myname)   
+console.log(myfriendname)
+
+
+let userone = {
+    email: "abc@gmail.com",
+    id: 12345
+
+}
+
+let usertwo = userone
+
+usertwo.email = "hpc@gmail.com"
+
+/*
+   In heap reference will be given so
+   it will also change the original value 
+   if you change the value in new object
+
+*/
+
+console.log(userone.email)
+console.log(usertwo.email)
+
+
+
