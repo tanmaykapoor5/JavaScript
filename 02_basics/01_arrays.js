d63f2e10ffae31c8329a4d2cb2955ab3d378aec0
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-09-08 17:27:06 +0000

    Arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// Array
+
+const myArr = [0,1,2,3,4,5];
+
+const myArr2 = new Array(1,2,3,4,5);
+
+console.log(myArr[0]);
+console.log(myArr2[0]);
+
+// Array Methods
+
+myArr.push(6);
+console.log(myArr);
