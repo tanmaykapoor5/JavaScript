797fd6a0b9a75ef11e4bb55d2bccf1dae3d79d46
Tanmay Kapoor <tanmaykapaaa567@gmail.com>
2026-09-20 17:52:47 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,10 @@
+// Immediately invoked function expressions (IIFE)
+// named iife
+( function chai() {
+    console.log(`hello`);
+})();
+
+// without name iife
+( () => {
+    console.log(`Tanmay`);
+})()
